dba09fd05f56cc7af886b16206add6da060e7a25
r4bb1t <r4bb1t@korea.ac.kr>
2019-11-27 15:02:40 +0900

    revert

--- index.js
+++ index.js
@@ -1,86 +1,103 @@
 function draw(max, classname, colorname){
-   var i = 1;
-    var func1 = setInterval(function(){
-      if(i<max){
-          color1(i, classname, colorname, max);
-          i++;
-    } else{
-        clearInterval(func1);
-    }
-    },10);
-}
-
-function color1(i, classname,colorname, max){
-   $(classname).css({
-        "background":"conic-gradient("+colorname+" 0% "+i+"%, gainsboro "+i+"% 100%)",
-   });
-}
-
-function play(){
-    draw(39, '.pie-chart1', 'crimson');
-}
-
-///////////////////////
-
-var flag = 0;
-
-$(window).on("scroll", function() {
-    var st = ($(window).scrollTop() - 400) / 2 - 10;
-    var st2 = 100 - ($("#statistics .s1 .text").position().top - 400 - $(window).scrollTop());
-    if ($(window).scrollTop() < 600 && $(window).scrollTop() > 400) {
-        $(".header-container").css({
-            "opacity": st + "%"
-        });
-    } else if ($(window).scrollTop() <= 400) {
-        $(".header-container").css({
-            "opacity": "0%"
-        });
-    } else {
-        $(".header-container").css({
-            "opacity": "90%"
-        });
-    }
-    /////
-    if ($(window).scrollTop() > $("#statistics").position().top - 200 && $(window).scrollTop() < $("#statistics").position().top + 300 && flag == 0) {
-        play();
-        flag = 1;
-    } else if ($(window).scrollTop() > $("#statistics").position().top + 500 || $(window).scrollTop() < $("#statistics").position().top - 500) {    
-        $(".pie-chart1").css({
-            "background":"conic-gradient(crimson 0% 0%, gainsboro 0% 100%)"
-        });
-        flag = 0;
-    }
-    /////
-    if ($(window).scrollTop() < $("#statistics .s1 .text").position().top - 400) {
-        $("#statistics .s1 .text").css({
-            "opacity": st2 + "%"
-        });
-    } else {
-        $("#statistics .s1 .text").css({
-            "opacity": "100%"
-        });
-    }
-    ///
-    if ($(window).scrollTop() >= $("#help").position().top - 10) {
-        $("a").css({
-            "color": "black"
-        });
-        $(".a-help").css({
-            "color": "crimson"
-        });
-    } else if ($(window).scrollTop() >= $("#danger").position().top - 10) {
-        $("a").css({
-            "color": "black"
-        });
-        $(".a-danger").css({
-            "color": "crimson"
-        });
-    } else if ($(window).scrollTop() >= $("#statistics").position().top - 10) {
-        $("a").css({
-            "color": "black"
-        });
-        $(".a-statistics").css({
-            "color": "crimson"
-        });
-    }
-});
+    var i = 1;
+     var func1 = setInterval(function(){
+       if(i<max){
+           color1(i, classname, colorname, max);
+           i++;
+     } else{
+         clearInterval(func1);
+     }
+     },10);
+ }
+ 
+ function color1(i, classname,colorname, max){
+    $(classname).css({
+         "background":"conic-gradient("+colorname+" 0% "+i+"%, gainsboro "+i+"% 100%)",
+    });
+ }
+ 
+ function play(){
+     draw(39, '.pie-chart1', 'palevioletred');
+     draw(50.6, '.pie-chart2', 'palevioletred');
+ }
+ 
+ ///////////////////////
+ 
+ var flag = 0;
+ 
+ $(window).on("scroll", function() {
+     var st = ($(window).scrollTop() - 400) / 2 - 10;
+     var st2 = 100 - ($("#statistics .chart-section").position().top - 400 - $(window).scrollTop());
+     var st3 = 100 - ($("#statistics .s1 .text").position().top - 400 - $(window).scrollTop());
+     if ($(window).scrollTop() < 600 && $(window).scrollTop() > 400) {
+         $(".header-container").css({
+             "opacity": st + "%"
+         });
+     } else if ($(window).scrollTop() <= 400) {
+         $(".header-container").css({
+             "opacity": "0%"
+         });
+     } else {
+         $(".header-container").css({
+             "opacity": "90%"
+         });
+     }
+     /////
+     if ($(window).scrollTop() > $("#statistics").position().top - 200 && $(window).scrollTop() < $("#statistics").position().top + 300 && flag == 0) {
+         play();
+         flag = 1;
+     } else if ($(window).scrollTop() > $("#statistics").position().top + 500 || $(window).scrollTop() < $("#statistics").position().top - 500) {    
+         $(".pie-chart1").css({
+             "background":"conic-gradient(palevioletred 0% 0%, gainsboro 0% 100%)"
+         });
+         flag = 0;
+     }
+     /////
+     if ($(window).scrollTop() < $("#statistics .chart-section").position().top - 400) {
+         $("#statistics .chart-section").css({
+             "opacity": st2 + "%"
+         });
+         $("#statistics .chart-title").css({
+             "opacity": st2 + "%"
+         });
+     } else {
+         $("#statistics .chart-section .text").css({
+             "opacity": "100%"
+         });
+         $("#statistics .chart-title").css({
+             "opacity": "100%"
+         });
+     }
+     if ($(window).scrollTop() < $("#statistics .s1 .text").position().top - 400) {
+         $("#statistics .s1 .text").css({
+             "opacity": st3 + "%"
+         });
+     } else {
+         $("#statistics .s1 .text").css({
+             "opacity": "100%"
+         });
+     }
+     ///
+     if ($(window).scrollTop() >= $("#help").position().top - 10) {
+         $("a").css({
+             "color": "black"
+         });
+         $(".a-help").css({
+             "color": "palevioletred"
+         });
+     } else if ($(window).scrollTop() >= $("#danger").position().top - 10) {
+         $("a").css({
+             "color": "black"
+         });
+         $(".a-danger").css({
+             "color": "palevioletred"
+         });
+     } else if ($(window).scrollTop() >= $("#statistics").position().top - 10) {
+         $("a").css({
+             "color": "black"
+         });
+         $(".a-statistics").css({
+             "color": "palevioletred"
+         });
+     }
+ });
