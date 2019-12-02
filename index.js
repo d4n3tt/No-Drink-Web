9864e2522633bca48df1ce470ef9e6ea473d1e50
r4bb1t <danettofficial@gmail.com>
2019-12-02 20:06:52 +0900

    hotfix

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function draw(max, classname, colorname){
     $(".title img").css({
         "opacity":i+"%"
     });
- }, 3d0);
+ }, 30);
 
  $(document).ready(function() {
     var agent = navigator.userAgent.toLowerCase();
