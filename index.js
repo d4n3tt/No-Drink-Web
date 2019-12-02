94959461cc7d6c6641ea4367f4b439e3fa9315a7
r4bb1t <danettofficial@gmail.com>
2019-12-02 13:55:24 +0900

    test table

--- index.js
+++ index.js
@@ -23,7 +23,21 @@ function draw(max, classname, colorname){
  
  ///////////////////////
  
- var flag = 0;
+ var flag = 0,  i = 0, t = 0;
+
+ textClick = setInterval(function() {
+    if (t == 0) {
+        i++;
+        if (i == 100) t = 1;
+    }
+    if (t == 1){
+        i--;
+        if (i == 0) t = 0;
+    }
+    $(".text-click").css({
+        "opacity":i+"%"
+    });
+ }, 5);
 
  $(document).ready(function() {
     var agent = navigator.userAgent.toLowerCase();
@@ -34,13 +48,15 @@ function draw(max, classname, colorname){
     }
  });
  
+ 
  $(window).on("scroll", function() {
      var st = ($(window).scrollTop() - 1600) / 2 - 10;
      var st2 = 100 - ($("#statistics .chart-section").position().top - 400 - $(window).scrollTop());
      var st3 = 100 - ($("#statistics .s1 .text").position().top - 400 - $(window).scrollTop());
      var st4 = 100 - ($("#danger").position().top - 200 - $(window).scrollTop());
-     var st5 = 100 - ($("#help .s119").position().top - 400 - $(window).scrollTop());
-     var st6 = 100 - ($("#help .text").position().top - 400 - $(window).scrollTop());
+     var st5 = 100 - ($("#test .test-table").position().top - 400 - $(window).scrollTop());
+     var st6 = 100 - ($("#help .s119").position().top - 400 - $(window).scrollTop());
+     var st7 = 100 - ($("#help .text").position().top - 400 - $(window).scrollTop());
      if ($(window).scrollTop() < 1800 && $(window).scrollTop() > 1600) {
          $(".header-container").css({
              "opacity": st + "%"
@@ -90,17 +106,26 @@ function draw(max, classname, colorname){
          });
      }
      if ($(window).scrollTop() < $("#danger").position().top - 200) {
-         $("#danger").css({
-             "opacity": st4 + "%"
-         });
-     } else {
-         $("#danger").css({
-             "opacity": "100%"
-         });
-     }
-     if ($(window).scrollTop() < $("#help .s119").position().top - 400) {
+        $("#danger").css({
+            "opacity": st4 + "%"
+        });
+    } else {
+        $("#danger").css({
+            "opacity": "100%"
+        });
+    }
+    if ($(window).scrollTop() < $("#test .test-table").position().top - 200) {
+        $("#test .test-table").css({
+            "opacity": st5 + "%"
+        });
+    } else {
+        $("#test .test-table").css({
+            "opacity": "100%"
+        });
+    }
+    if ($(window).scrollTop() < $("#help .s119").position().top - 400) {
          $("#help .s119").css({
-             "opacity": st5 + "%"
+             "opacity": st6 + "%"
          });
      } else {
          $("#help .s119").css({
@@ -109,7 +134,7 @@ function draw(max, classname, colorname){
      }
      if ($(window).scrollTop() < $("#help .text").position().top - 400) {
          $("#help .text").css({
-             "opacity": st6 + "%"
+             "opacity": st7 + "%"
          });
      } else {
          $("#help .text").css({
@@ -122,9 +147,16 @@ function draw(max, classname, colorname){
              "color": "black"
          });
          $(".a-help").css({
-             "color": "#9370D8"
+             "color": "#7B70EF"
          });
-     } else if ($(window).scrollTop() >= $("#danger").position().top - 50) {
+     } else if ($(window).scrollTop() >= $("#test").position().top - 50) {
+        $("header a").css({
+            "color": "black"
+        });
+        $(".a-test").css({
+            "color": "#9370D8"
+        });
+    } else if ($(window).scrollTop() >= $("#danger").position().top - 50) {
          $("header a").css({
              "color": "black"
          });
@@ -143,18 +175,43 @@ function draw(max, classname, colorname){
 
  ////
 
- console.log($(".icon-container").position().top);
- console.log($(".detail").position().top);
  $(".detail h4").text("알코올을 섭취하지 않으면 잠을 잘 수 없는 알코올 의존성 수면장애나 불면증을 겪을 수 있습니다. 알코올의존증 환자의 불면증 유병율은 연구에 따라 36~72%에 육박합니다. 또 알코올은 각성 시에 나타나는 뇌파를 수면 중에 증가시키고 신체회복에 중요한 역할을 하는 렘 수면 또한 차단하여 일어난 후에도 피로를 느끼게 합니다.");
- function mover1(){
+ $(".icon1").css({
+    "transform": "scale(1.1)"
+ })
+function mover1(){
+    $(".icon").css({
+        "transform": "scale(1.0)"
+    })
+    $(".icon1").css({
+        "transform": "scale(1.1)"
+    })
     $(".detail h4").text("알코올을 섭취하지 않으면 잠을 잘 수 없는 알코올 의존성 수면장애나 불면증을 겪을 수 있습니다. 알코올의존증 환자의 불면증 유병율은 연구에 따라 36~72%에 육박합니다. 또 알코올은 각성 시에 나타나는 뇌파를 수면 중에 증가시키고 신체회복에 중요한 역할을 하는 렘 수면 또한 차단하여 일어난 후에도 피로를 느끼게 합니다.");
 }
 function mover2(){
+    $(".icon").css({
+        "transform": "scale(1.0)"
+    })
+    $(".icon2").css({
+        "transform": "scale(1.1)"
+    })
     $(".detail h4").text("음주 전후 약물을 섭취하면 알코올이 약물의 대사을 저해하여 약물농도를 높이거나 독성물질의 발생을 촉진해 약물의 부작용의 발현 빈도나 강도를 상승시킬 수 있고, 반대로 약물의 분해를 촉진하여 약효를 떨어뜨릴 수도 있습니다. 위염 등 위장장애 및 위장출혈, 간 손상, 저혈압 등과 같은 부작용을 겪을 수 있습니다.\n");
 }
 function mover3(){
+    $(".icon").css({
+        "transform": "scale(1.0)"
+    })
+    $(".icon3").css({
+        "transform": "scale(1.1)"
+    })
     $(".detail h4").text("과다한 음주는 간질환을 유발합니다.\n무증상 지방간에서 알코올성 간염, 간경변, 말기 간부전에 이르는 다양한 질환에 노출될 수 있습니다.");
 }
 function mover4(){
+    $(".icon").css({
+        "transform": "scale(1.0)"
+    })
+    $(".icon4").css({
+        "transform": "scale(1.1)"
+    })
     $(".detail h4").text("세계보건기구(WTO)는 술을 1급 발암 물질로 지정하고 술을 마시면 마실수록 암 발생 위험이 높아진다고 경고했습니다.\n인체가 흡수한 발암 물질을 녹여 점막이나 인체 조직 등에 쉽게 침투하도록 돕는 역할을 합니다.\n알코올이 몸에서 흡수되는 과정에서 생성되는 아세트알데히드 역시 암을 일으킬 수 있습니다.");
 }
